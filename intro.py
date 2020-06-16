239b7a6820187e6de4641daa9c8a010ea4a9efa5
Ning Wang <nwpigm5@gmail.com>
2020-06-16 15:36:58 -0700

    Initial commit

---
+++ intro.py
@@ -0,0 +1,21 @@
+import math
+
+import pandas as pd
+import quandl
+
+df = quandl.get('WIKI/GOOGL') # DF for Data Frame
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100
+df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+print(df.tail())
