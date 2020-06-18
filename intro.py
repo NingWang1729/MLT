d6cc28299b16799d39c6f9ff1313a824281871ad
Ning Wang <nwpigm5@gmail.com>
2020-06-18 16:17:54 -0700

    Tests Accuracy of Linear Regression

--- intro.py
+++ intro.py
@@ -1,9 +1,10 @@
-import math
-
 import pandas as pd
-import quandl
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, svm, model_selection
+from sklearn.linear_model import LinearRegression
 
-df = quandl.get('WIKI/GOOGL') # DF for Data Frame
+df = quandl.get('WIKI/GOOGL')  # DF for Data Frame
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100
 df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
@@ -13,9 +14,19 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
-print(df.tail())
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
